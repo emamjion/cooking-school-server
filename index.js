83210df980afa51d6519c791c9432d585397dbb9
emamjion <emamjion@gmail.com>
2023-06-12 14:27:37 +0600

    my selected class running

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -66,6 +66,13 @@ async function run() {
         const item = req.body;
         console.log(item);
         const result = await bookedCollection.insertOne(item);
+        res.send(result);
+    });
+
+    app.delete('/booked/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id : new ObjectId(id)};
+        const result = await bookedCollection.deleteOne(query);
         res.send(result);
     })
     
